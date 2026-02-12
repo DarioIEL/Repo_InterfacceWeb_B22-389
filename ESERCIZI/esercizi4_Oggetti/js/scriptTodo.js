14e1c0ddfe1eb058c311935da42f23aee60d02f8
= <=>
2026-02-12 15:51:45 +0100

    aggiornamento Todo

--- ESERCIZI/esercizi4_Oggetti/js/scriptTodo.js
+++ ESERCIZI/esercizi4_Oggetti/js/scriptTodo.js
@@ -7,6 +7,8 @@ class Item{
 
 const formTodo = document.getElementById("formTodo");
 const elListaItems = document.getElementById("elListaItems");
+const feed = document.getElementById("feed");
+
 
 let items = [];
 
@@ -14,11 +16,29 @@ function aggiungiItem(){
     let cosa = formTodo.cosaItem.value;
     let data = formTodo.dataItem.value;
 
+    if(!cosa.trim() || !data){
+        alert("Compila tutti i campi");
+        return;
+    }
+
+    //Gestione Duplicati
+    //il metodo find restituisce sempre un oggetto
+    let dataDuplicata = items.find(item => item.data == data);
+    if(dataDuplicata){
+        feed.innerHTML = "Attenzione, hai già un'altra cosa da fare in quella data";
+
+        setTimeout(() => {
+            feed.innerHTML = "";
+        }, 2000);
+    }
+    
+
     let nuovoItem = new Item(cosa, data);
 
     items.push(nuovoItem);
 
     if(items.length != 0){
+        items = ordinaTODO();
         stampaItems();
     }
 
@@ -28,8 +48,24 @@ function stampaItems(){
     elListaItems.innerHTML = "";
 
     items.forEach(item =>{
-        elListaItems.innerHTML += "<li>" + item.cosaDaFare + " - " + item.data + "</li>";
+        elListaItems.innerHTML += "<li>" + item.cosaDaFare + " - " + formattaData(item.data) + " <button>X</button> </li>";
     })
+}
+
+function formattaData(dateString){
+    let data = new Date(dateString);
+    let giorno = data.getDate();
+    let mese = data.getMonth() + 1;
+    let anno = data.getFullYear();
+
+    let dataFinale = giorno + "/" + mese + "/" + anno;
+
+    return dataFinale;
+}
+
+
+function ordinaTODO(){
+    return [...items].sort( (i,f) => new Date(i.data) - new Date(f.data) );
 }
 
 formTodo.addEventListener("submit", function(event){
