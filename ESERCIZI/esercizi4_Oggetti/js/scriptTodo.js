ba9cb6f833e39b7c41023727494bcbe33a0d044e
= <=>
2026-02-12 15:18:11 +0100

    uodate Lezione 12/02

--- ESERCIZI/esercizi4_Oggetti/js/scriptTodo.js
+++ ESERCIZI/esercizi4_Oggetti/js/scriptTodo.js
@@ -1,2 +1,39 @@
-let date = new Date()
+class Item{
+    constructor(cosaDaFare, data){
+        this.cosaDaFare = cosaDaFare;
+        this.data = data;
+    }
+}
+
+const formTodo = document.getElementById("formTodo");
+const elListaItems = document.getElementById("elListaItems");
+
+let items = [];
+
+function aggiungiItem(){
+    let cosa = formTodo.cosaItem.value;
+    let data = formTodo.dataItem.value;
+
+    let nuovoItem = new Item(cosa, data);
+
+    items.push(nuovoItem);
+
+    if(items.length != 0){
+        stampaItems();
+    }
+
+}
+
+function stampaItems(){
+    elListaItems.innerHTML = "";
+
+    items.forEach(item =>{
+        elListaItems.innerHTML += "<li>" + item.cosaDaFare + " - " + item.data + "</li>";
+    })
+}
+
+formTodo.addEventListener("submit", function(event){
+    event.preventDefault();
+    aggiungiItem();
+});
 
